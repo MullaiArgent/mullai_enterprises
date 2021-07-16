f9a487159a4b3aeefa3d6d881de91281733efb45
MullaiArgent <mullairajan2000@gmail.com>
2021-07-16 23:16:22 +0530

    Admin Panel Update

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns=[
     path('',views.home, name='home'),
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
